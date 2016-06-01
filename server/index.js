22a4da608bf3e4893487dd81e21428280da2b006
Dan Zajdband <dan.zajdband@gmail.com>
2016-06-01 16:06:48 -0400

    Added s3 option

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ var cors = require('cors')
 var express = require('express')
 var bodyParser = require('body-parser')
 var compress = require('compression')
+var AWS = require('aws-sdk')
 var rollup = require('rollup')
 var babel = require('rollup-plugin-babel')
 var nodeResolve = require('rollup-plugin-node-resolve')
@@ -16,6 +17,17 @@ var axios = require('axios')
 var config = require('../config.json')
 axios.defaults.baseURL = config.pillarHost
 axios.defaults.headers.common['Authorization'] = config.basicAuthorization
+
+var isS3 = config.s3 && config.s3.bucket
+var s3bucket
+if (isS3) {
+  AWS.config.update({
+    region: config.s3.region,
+    accessKeyId: config.s3.accessKeyId,
+    secretAccessKey: config.s3.secretAccessKey
+  })
+  s3bucket = new AWS.S3({params: {Bucket: config.s3.bucket}});
+}
 
 var app = express()
 app.use(cors())
@@ -40,10 +52,27 @@ app.get('/preview.js', function(req, res) {
 app.post('/create', function(req, res) {
   axios.post('/api/form', req.body)
     .then(function(response) {
-      buildWidget(response.data, false).then(function(code){
-        fs.writeFile(path.join(__dirname, 'widgets', response.data.id + '.js'), code, function(err) {
-          res.json(response.data)
-        })
+      buildWidget(response.data, false).then(function(code) {
+        var key = response.data.id + '.js'
+        if (isS3) {
+          var params = {Bucket: config.s3.bucket, Key: key, Body: code}
+          s3bucket.upload(params, function(err, data) {
+            if(err) {
+              console.log(err)
+              return res.status(500).send('Error while uploading to s3')
+            }
+
+            res.send(response.data)
+          })
+        } else {
+          fs.writeFile(path.join(__dirname, 'widgets', key), code, function(err) {
+            if (err) {
+              return res.status(500).send('Error while saving file')
+            }
+
+            res.json(response.data)
+          })
+        }
       })
       .catch(function(err){ res.status(500).send(err.stack) })
     })
