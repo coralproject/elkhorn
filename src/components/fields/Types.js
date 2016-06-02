bf1b6d08e147b443e7a9c1142c344bcaca7930ce
Pablo Cubico <pablocuadrado@gmail.com>
2016-06-02 12:41:02 -0300

    Unused types removed from the build

--- src/components/fields/Types.js
+++ src/components/fields/Types.js
@@ -1,20 +1,10 @@
 
-import Rating from './Rating';
-import Audio from './Audio';
 import TextArea from './TextArea';
 import TextField from './TextField';
-import Drawer from './Drawer';
-import ImageOptions from './ImageOptions';
-import MultipleChoice from './MultipleChoice';
 import LocationDropdown from './LocationDropdown';
 
 export {
-  Rating,
-  Audio,
   TextArea,
   TextField,
-  Drawer,
-  ImageOptions,
-  MultipleChoice,
   LocationDropdown
 };
