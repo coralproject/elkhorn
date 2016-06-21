2e77e5979dc801862ff2fc83e27e30a018045370
Pablo Cubico <pablocuadrado@gmail.com>
2016-06-21 15:21:00 -0300

    Readding multiple choice to field types

--- src/components/fields/Types.js
+++ src/components/fields/Types.js
@@ -2,9 +2,11 @@
 import TextArea from './TextArea';
 import TextField from './TextField';
 import LocationDropdown from './LocationDropdown';
+import MultipleChoice from './MultipleChoice';
 
 export {
   TextArea,
   TextField,
-  LocationDropdown
+  LocationDropdown,
+  MultipleChoice
 };
