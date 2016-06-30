54aacfd80dc53090de442a6966fb97859fde9aa2
Dan Zajdband <dan.zajdband@gmail.com>
2016-06-30 11:55:13 -0400

    Added date picker

--- src/components/fields/Types.js
+++ src/components/fields/Types.js
@@ -1,12 +1,14 @@
 
 import TextArea from './TextArea';
 import TextField from './TextField';
+import DateField from './DateField';
 import LocationDropdown from './LocationDropdown';
 import MultipleChoice from './MultipleChoice';
 
 export {
   TextArea,
   TextField,
+  DateField,
   LocationDropdown,
   MultipleChoice
 };
